4f418927f83c72c39a78b6d161ba12f3298a3f9c
Samuel Kris <118498282+Samkris17@users.noreply.github.com>
2024-08-18 00:47:29 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/krist_19"
+                href="https://www.instagram.com/_krist19"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
